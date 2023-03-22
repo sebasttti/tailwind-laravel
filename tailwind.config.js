c14b7552358089e2cc671e7b2a4db35197786172
sebasttti <juansjoya_r@outlook.com>
2023-03-21 21:12:07 -0500

    Grid

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        container: { center: true },
+        extend: {},
+    },
+    plugins: [],
+};
